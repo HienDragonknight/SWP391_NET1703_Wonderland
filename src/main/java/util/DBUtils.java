fdc02d83c875d619de002ff1bae793058a7c273c
huY-it-now <lehuy15062003@gmail.com>
2024-01-20 14:22:58 +0700

    change db

--- src/main/java/util/DBUtils.java
+++ src/main/java/util/DBUtils.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  */
 public class DBUtils {
     
-    private static final String DB_NAME = "PartyForKids";
+    private static final String DB_NAME = "Wonderland";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASS = "12345";
     
