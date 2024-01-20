2c09e8e60d32b2522a50333c98b1770db498d15f
baopen753 <tranquocbao070503@gmail.com>
2024-01-20 20:41:05 +0700

    create userDTO

--- src/main/java/models/UserDTO.java
+++ src/main/java/models/UserDTO.java
@@ -10,4 +10,73 @@ package models;
  */
 public class UserDTO {
     
+       private String fullName;
+       private String email;
+       private String password;
+       private String phoneNumber;
+       private String avatar;
+       private int roleID;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String fullName, String email, String password, String phoneNumber, String avatar, int roleID) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.avatar = avatar;
+        this.roleID = roleID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+       
+       
+    
 }
