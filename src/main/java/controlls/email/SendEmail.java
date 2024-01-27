e455eb2db6864ea098e2ee35d8977142fa7f0ca3
baopen753 <tranquocbao070503@gmail.com>
2024-01-27 14:22:02 +0700

    update order page

---
+++ src/main/java/controlls/email/SendEmail.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlls.email;
+
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author Hp
+ */
+public class SendEmail {
+
+    public String getRandom() {
+        Random rnd = new Random();
+        int code = rnd.nextInt(999999);
+        return String.format("%06d", code);
+    }
+
+}
