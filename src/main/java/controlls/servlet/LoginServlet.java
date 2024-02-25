e200c46b12ff28a9396be54dbb2fe57ddb403c7d
TamNPT <tampt93@gmail.com>
2024-02-25 15:55:35 +0700

    error login

--- src/main/java/controlls/servlet/LoginServlet.java
+++ src/main/java/controlls/servlet/LoginServlet.java
@@ -36,7 +36,7 @@ public class LoginServlet extends HttpServlet {
         System.out.println(contextPath);
 
         HttpSession session = request.getSession();
-        session.setAttribute("ERROR_INFO", "Incorrect username or password");
+        session.removeAttribute("ERROR_INFO"); // Remove any previous error messages
 
         String url = LOGIN_PAGE;
         String button = request.getParameter("action");
@@ -76,6 +76,9 @@ public class LoginServlet extends HttpServlet {
                     response.addCookie(cRemember);
                     url = HOME_PAGE;
                     session.setAttribute("USER_INFO", result);
+                } else {
+                    // If login fails, set error message
+                    session.setAttribute("ERROR_INFO", "Incorrect username or password");
                 }
             }
         } catch (ClassNotFoundException e) {
@@ -83,7 +86,6 @@ public class LoginServlet extends HttpServlet {
         } catch (SQLException e) {
             log("CreateAccountServlet _ SQL: " + e.getMessage());
         } finally {
-            //response.sendRedirect(url);
             request.getRequestDispatcher(url).forward(request, response);
         }
     }
